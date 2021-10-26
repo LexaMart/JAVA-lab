282f5d568abe74cf5a0dd8393c252d9b585d3da2
Alex <lexamart1708@gmail.com>
2021-10-26 17:27:39 +0300

    try to use consol

---
+++ src/MainApplication.java
@@ -0,0 +1,20 @@
+
+import javax.swing.JFrame;
+
+public class MainApplication
+{
+    public static void main(String[]args)
+    {
+       /* JFrame frame = new JFrame();
+        frame.setSize(500,500);
+        JButton b1 = new JButton("НАЖМИ МЕНЯ");
+        frame.add(b1);
+        frame.setTitle(" Мое первое окошко :) ");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);*/
+        Main_Frame frame = new Main_Frame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
